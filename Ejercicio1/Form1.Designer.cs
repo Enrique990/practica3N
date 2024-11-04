79bd432fea734d4b8fe3164fa455d9a2e3af3171
Enrique990 <voidenrique@gmail.com>
2024-11-04 08:34:58 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Ejercicio1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblContraseña = new System.Windows.Forms.Label();
+            this.tbUsuario = new System.Windows.Forms.TextBox();
+            this.tbContraseña = new System.Windows.Forms.TextBox();
+            this.btnIniciarSesión = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(43, 48);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(54, 16);
+            this.lblUsuario.TabIndex = 0;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // lblContraseña
+            // 
+            this.lblContraseña.AutoSize = true;
+            this.lblContraseña.Location = new System.Drawing.Point(21, 89);
+            this.lblContraseña.Name = "lblContraseña";
+            this.lblContraseña.Size = new System.Drawing.Size(76, 16);
+            this.lblContraseña.TabIndex = 1;
+            this.lblContraseña.Text = "Contraseña";
+            // 
+            // tbUsuario
+            // 
+            this.tbUsuario.Location = new System.Drawing.Point(144, 48);
+            this.tbUsuario.Name = "tbUsuario";
+            this.tbUsuario.Size = new System.Drawing.Size(241, 22);
+            this.tbUsuario.TabIndex = 2;
+            // 
+            // tbContraseña
+            // 
+            this.tbContraseña.Location = new System.Drawing.Point(144, 89);
+            this.tbContraseña.Name = "tbContraseña";
+            this.tbContraseña.Size = new System.Drawing.Size(241, 22);
+            this.tbContraseña.TabIndex = 3;
+            // 
+            // btnIniciarSesión
+            // 
+            this.btnIniciarSesión.Location = new System.Drawing.Point(286, 133);
+            this.btnIniciarSesión.Name = "btnIniciarSesión";
+            this.btnIniciarSesión.Size = new System.Drawing.Size(99, 25);
+            this.btnIniciarSesión.TabIndex = 4;
+            this.btnIniciarSesión.Text = "Iniciar sesión";
+            this.btnIniciarSesión.UseVisualStyleBackColor = true;
+            this.btnIniciarSesión.Click += new System.EventHandler(this.btnIniciarSesión_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 174);
+            this.Controls.Add(this.btnIniciarSesión);
+            this.Controls.Add(this.tbContraseña);
+            this.Controls.Add(this.tbUsuario);
+            this.Controls.Add(this.lblContraseña);
+            this.Controls.Add(this.lblUsuario);
+            this.Name = "Form1";
+            this.Text = "LoginForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblContraseña;
+        private System.Windows.Forms.TextBox tbUsuario;
+        private System.Windows.Forms.TextBox tbContraseña;
+        private System.Windows.Forms.Button btnIniciarSesión;
+    }
+}
+
---
+++ Ejercicio1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciarSesión_Click(object sender, EventArgs e)
+        {
+            if (tbUsuario.Text == "" || tbContraseña.Text == "")
+            {
+                MessageBox.Show("Debe completar ambos campos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (tbUsuario.Text == "admin" || tbContraseña.Text == "1234")
+            {
+                MessageBox.Show("Bienvenido Administrador");
+                return; 
+            }
+            MessageBox.Show("Bienvenido " + tbUsuario.Text);
+        }
+    }
+}
